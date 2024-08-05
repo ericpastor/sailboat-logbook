df770b9fe86bcb69886459ac310d203f59e9c69d
ericpastor <ericpastorpadial@gmail.com>
2024-08-05 10:41:20 +0300

    Cypress installed

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,14 @@
+import { italianno } from "@/app/fonts"
 import styles from "./footer.module.css"
 
 
 export default function Footer() {
     return (
         <div className={styles.container}>
-            <div className={styles.logo}>Footer</div>
             <div className={styles.logo}>Captain&apos;s Logbook 2024</div>
+            <div className={italianno.variable}>
+                <div className={styles.text}>Sailing the 7 Seas</div>
+            </div>
         </div>
     )
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import Links from "./links/Links";
 import styles from "./navbar.module.css"
+import Image from "next/image";
 
 export default function Navbar() {
     return (
         <div className={styles.container}>
-            <Link href="/" className={styles.logo}>LOGO</Link>
+            <Link href="/" className={styles.logo}>  <Image src="/sailing.png" alt="icon sailboat" width={50} height={50}></Image></Link>
             <div>
                 <Links />
             </div>
